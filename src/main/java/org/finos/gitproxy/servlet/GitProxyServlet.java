24339c29ac58d05f88987aa903ad904f0bfb7563
Thomas Cooper <thomas.cooper@rbc.com>
2025-06-27 13:03:59 -0400

    experiment: use jetty AsyncProxyServlet

--- src/main/java/org/finos/gitproxy/servlet/GitProxyServlet.java
+++ src/main/java/org/finos/gitproxy/servlet/GitProxyServlet.java
@@ -3,15 +3,16 @@ package org.finos.gitproxy.servlet;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.client.BytesRequestContent;
 import org.eclipse.jetty.client.Request;
-import org.eclipse.jetty.ee10.proxy.ProxyServlet;
+import org.eclipse.jetty.ee10.proxy.AsyncProxyServlet;
 import org.finos.gitproxy.git.GitRequestDetails;
 
+import java.io.IOException;
+
 @Slf4j
-public class GitProxyServlet extends ProxyServlet.Transparent {
+public class GitProxyServlet extends AsyncProxyServlet.Transparent {
     public static final String GIT_REQUEST_ATTRIBUTE = "org.finos.gitproxy.gitproxy.gitRequest";
 
     @Override
